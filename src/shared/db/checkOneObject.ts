69d72c55c649fd8b08e6d0a492d559ae946e9826
Flayka <the.flayka@gmail.com>
2025-10-02 18:27:01 +0500

    FirstStepToCreateSystem

--- src/shared/db/checkOneObject.ts
+++ src/shared/db/checkOneObject.ts
@@ -1,7 +1,15 @@
 // MongoDb
+import { ObjectId } from 'mongodb'
 import { clientPromise } from '#src/mongodb.js'
 import { sendErrorResponse, sendSuccessResponse } from '#shared/response/sendResponse.js'
 import { AllResponse } from '#types/response.types.js'
+import { isSuccessResponse } from '#shared/isSuccess.js'
+
+// Functions
+import { decodeJWT } from '#shared/jwtDecode.js'
+
+// Types
+import { IModuleType } from '#types/module.types.js'
 
 export const checkOneObject = async (
   collection: string,
@@ -22,4 +30,19 @@ export const checkOneObject = async (
   } catch (error) {
     return sendErrorResponse('Ошибка при нахождений объекта', 500, error)
   }
+}
+
+export const newModelFunction = async <TData, TBody = undefined>(
+  fn: (data: TData, body: TBody) => Promise<AllResponse>, module: IModuleType,
+  token: string, body?: TBody
+) => {
+  const resultCheck = await checkOneObject(
+    module.type,
+    {
+      _id: ObjectId.createFromHexString(decodeJWT(token)._id),
+    },
+    module.case
+  )
+  if (!isSuccessResponse<TData>(resultCheck)) return resultCheck
+  return fn(resultCheck.data, body as TBody)
 }
